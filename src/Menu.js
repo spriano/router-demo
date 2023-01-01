a28009a34f34ca52bbb9f4698550bb8330555e63
aldospriano <aldo.spriano.alice.it>
2023-01-01 21:39:40 +0100

    link a destra

--- src/Menu.js
+++ src/Menu.js
@@ -1,19 +1,21 @@
-import { Nav, Navbar,NavItem,NavDropdown,Container } from "react-bootstrap";
+import { Nav, Navbar,Container } from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 function Menu() {
   return (
     
-      <Navbar bg="dark" variant="dark" expand='lg'>
+      <Navbar bg="primary" variant="dark" expand='md'>
         <Container>
           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-ullrightnavbar-nav">
-            <Nav  className="ml-auto">
-              <Nav.Link  Link to="/" >Home</Nav.Link>
-              <Nav.Link Link to="/about">About</Nav.Link>
-              <Nav.Link Link to="/contatti">Contatti</Nav.Link>
-            </Nav>
+            <Nav  className="ms-auto">
+            
+            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+                <Nav.Link as={Link} to={"/contatti"}>Contatti</Nav.Link>               
+             </Nav> 
+             
           </Navbar.Collapse>
         </Container>
       </Navbar>
