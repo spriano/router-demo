23ebc6a4d7a15eb7b49157c201c0ab268d87ee1d
aldospriano <aldo.spriano.alice.it>
2023-03-13 12:23:54 +0100

    finale

--- src/App.js
+++ src/App.js
@@ -2,37 +2,63 @@
 import './App.css'
 import Menu from './Menu'
 // import Menu1 from './Menu1'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import About from './components/About';
+ import Commenti from './components/Commenti'
+import Home from './components/Home';
+import Contatti from './components/Contatti' 
+
 
 
 function App() {
+  const menuData=[
+    {
+      path:"/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path:"/about",
+      name: 'About',
+      component: About
+    },
+    {
+      path:"/contatti",
+      name: 'Contatti',
+      component: Contatti
+    },
+    {
+      path:"/commenti",
+      name: "Commenti",
+      component: Commenti
+    }
+  ]
+  
+
   return (
    
     
        
       <div className="App">
+        <Router>
+        <Menu menuData={menuData}/>
         
-        <Menu/>
-        {/* <Navbar bg="dark" variant="dark" expand="lg">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-ullrightnavbar-nav">
-              <Nav className="ml-auto">
-              <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
-                <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
-                <Nav.Link as={Link} to={"/contatti"}>Contatti</Nav.Link>               
-             </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar> */}
 
-        {/* <Routes>
-          <Route path="/about" element={<About />} />           
-          <Route path="/contatti" element={<Contatti />} />         
-          <Route path="/" element={<Home />} />
+          <Routes>
+
+            {/* {menuData.map(page => (
+
+                <Route exact path={page.path} component={page.component} key={page.name} />
+                
+            ))} */}
             
-        </Routes> */}
-      
+            <Route path="/about" element={<About />} />           
+            <Route path="/contatti" element={<Contatti />} />         
+            <Route path="/" element={<Home />} />
+            <Route path="/commenti" element={<Commenti />} />
+    
+          </Routes>
+        </Router>
     </div>
 
   );
